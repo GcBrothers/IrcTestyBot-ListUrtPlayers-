8a15405e7dd744351d9cb8deced6fac413891374
GcBrothers <gcbrothersut@gmail.com>
2014-08-24 16:56:58 +0200

    Fixed Only Players

--- bot.py
+++ bot.py
@@ -21,25 +21,26 @@ class myIRCBot(ircbot.SingleServerIRCBot):
         if message[0] == "!": #Detect Command
             serv.privmsg("#LcD", "Command detected")
             command, arg = string.split(message, " ", 1)
-            if command == "!players":
-                adress, port = string.split(arg, ":", 1)
-                connexionServeur = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Creating connection
-                print "Sending Data"
-                connexionServeur.sendto("\xFF\xFF\xFF\xFF\x02getstatus\x0a\x00", (adress, port)) #Asking for data to the server
-                data, addr = connexionServeur.recvfrom(10240) #Receive data
-                #print data
-                print "Receiving data"
-                datas = string.split(data, "\\", 116) #Separating useless data
-                clientsToPurge = datas[116]
-                datas = string.split(clientsToPurge, "\n")
-                i = 1
-                while datas[i]: #Tell connected players
-                    client = string.split(datas[i], " ")
-                    serv.privmsg("#LcD", "%s a %s kills (%s de ping)"%(client[2], client[0], client[1]))
-                    i = i + 1
-                    time.sleep(0.7)
-                connexionServeur.close()
-                serv.privmsg("#LcD", "Done.")
+            if arg:
+                if command == "!players":
+                    adress, port = string.split(arg, ":", 1)
+                    connexionServeur = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Creating connection
+                    print "Sending Data"
+                    connexionServeur.sendto("\xFF\xFF\xFF\xFF\x02getstatus\x0a\x00", (adress, int(port))) #Asking for data to the server
+                    data, addr = connexionServeur.recvfrom(10240) #Receive data
+                    #print data
+                    print "Receiving data"
+                    datas = string.split(data, "\\", 116) #Separating useless data
+                    clientsToPurge = datas[116]
+                    datas = string.split(clientsToPurge, "\n")
+                    i = 1
+                    while datas[i]: #Tell connected players
+                        client = string.split(datas[i], " ")
+                        serv.privmsg("#LcD", "%s a %s kills (%s de ping)"%(client[2], client[0], client[1]))
+                        i = i + 1
+                        time.sleep(0.7)
+                    connexionServeur.close()
+                    serv.privmsg("#LcD", "Done.")
         
 if __name__ == "__main__":
     print 'Working'
